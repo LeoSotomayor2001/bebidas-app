96e77931749f43bf02ca9ff1e76060d0ea500cd1
Leodomi <Leodomi15@gmail.com>
2024-08-16 20:13:37 -0400

    agregando el boton y la funcionalidad de eliminar bebida

--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Spinner } from "../components/Spinner";
-
+import { Toast } from "primereact/toast";
+import Swal from "sweetalert2";
 export const Index = () => {
   const [bebidas, setBebidas] = useState([]);
   const [loading, setLoading] = useState(true);
-
+  const toast = useRef(null);
   const mostrarBebidas = async () => {
     try {
       const response = await fetch("http://127.0.0.1:8000/api/bebidas");
@@ -23,12 +24,46 @@ export const Index = () => {
   }, []);
 
   if (loading) {
-    return <Spinner/>
+    return <Spinner />;
   }
+  const deleteBebida = async (id) => {
+    Swal.fire({
+      title: "Estas seguro?",
+      text: "No podras revertir esta accion!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, Eliminar!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const response = await fetch(`http://127.0.0.1:8000/api/bebidas/${id}`, {
+            method: "DELETE",
+          });
+          const data = await response.json();
+          console.log(data)
+          toast.current.show({
+            severity: "success",
+            summary: "Bebida Eliminada",
+            life: 3000,
+          })
+          setTimeout(() => {
+            
+            mostrarBebidas();
+          }, 3000);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    })
+   
+  };
 
   return (
     <div className="container m-auto ">
-        <h1 className="text-center text-3xl my-2">Lista de bebidas</h1>
+      <Toast ref={toast} />
+      <h1 className="text-center text-3xl my-2">Lista de bebidas</h1>
       {bebidas.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-6 gap-4">
           {bebidas.map((bebida) => (
@@ -40,11 +75,17 @@ export const Index = () => {
                 src={`${import.meta.env.VITE_IMAGE_URL}${bebida.imagen}`}
                 alt={bebida.nombre}
                 className="w-full h-48 object-cover "
-
               />
               <div className="p-4">
                 <h2 className="text-xl font-bold">{bebida.nombre}</h2>
                 <p className="text-gray-600">{bebida.tipo}</p>
+                <button
+                  className="bg-red-500 text-white p-1 mt-2 rounded-lg"
+                  type="button"
+                  onClick={() => deleteBebida(bebida.id)}
+                >
+                  Borrar
+                </button>
               </div>
             </div>
           ))}
