90d8402d9d9bcdfb6fd593e6af30f45d7cb3ddae
Leodomi <Leodomi15@gmail.com>
2024-08-26 21:03:27 -0400

    agregada la funcion de registrar usuarios

--- src/views/Auth/RegisterPage.jsx
+++ src/views/Auth/RegisterPage.jsx
@@ -1,22 +1,98 @@
 import { FloatLabel } from "primereact/floatlabel";
 import { InputText } from "primereact/inputtext";
-import React from "react";
-import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import { Toast } from "primereact/toast";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const RegisterPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [password_confirmation, setPassword_confirmation] = useState('');
+  const [errors, setErrors] = useState([])
+  const toast = useRef(null);
+  const navigate = useNavigate();
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'name') setName(value);
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+    if (name === 'password_confirmation') setPassword_confirmation(value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setErrors([]);
+    // TODO: Crear cuenta
+    try {
+      const {data} = await axios.post(
+        'http://127.0.0.1:8000/api/register',
+        {
+          name,
+          email,
+          password,
+          password_confirmation,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      
+      if(data){
+        toast.current.show({severity:'success', summary: 'Cuenta creada', detail: data.message, life: 3000});
+        setName('');
+        setEmail('');
+        setPassword('');
+        setPassword_confirmation('');
+        setTimeout(() => {
+          navigate('/auth/login');
+        }, 1000);
+      }
+    } catch (error) {
+      console.log(error.response.data);
+      setErrors(error.response.data); 
+      setTimeout(() => {
+        setErrors([]);
+
+      }, 2000);
+    }
+  };
+  
   return (
-    <form className="border border-gray-200 p-6 mx-auto md:w-2/3 lg:w-3/4 shadow-xl rounded-xl bg-white">
+    <form
+      className="border border-gray-200 p-6 mx-auto md:w-2/3 lg:w-3/4 shadow-xl rounded-xl bg-white"
+      onSubmit={onSubmit}
+    >
+      <Toast ref={toast} />
       <h1 className="text-3xl font-bold text-center">Crear Cuenta</h1>
       <div className="my-5">
         <FloatLabel>
-          <InputText id="nombre" name="nombre" className="w-full" />
-          <label htmlFor="nombre">Nombre</label>
+          <InputText
+            id="name"
+            name="name"
+            className="w-full"
+            value={name}
+            onChange={onChange}
+          />
+          <label htmlFor="name">Nombre</label>
+          {errors.name && <p className="text-red-500">{errors.name[0]}</p>}
         </FloatLabel>
       </div>
       <div className="my-5">
         <FloatLabel>
-          <InputText id="email" name="email" className="w-full" type="email" />
+          <InputText
+            id="email"
+            name="email"
+            className="w-full"
+            type="email"
+            value={email}
+            onChange={onChange}
+          />
           <label htmlFor="email">Email</label>
+          {errors.email && <p className="text-red-500">{errors.email[0]}</p>}
         </FloatLabel>
       </div>
       <div className="my-5">
@@ -26,11 +102,13 @@ export const RegisterPage = () => {
             name="password"
             className="w-full"
             type="password"
+            value={password}
+            onChange={onChange}
           />
           <label htmlFor="password">Contraseña</label>
+          {errors.password && <p className="text-red-500">{errors.password[0]}</p>}
         </FloatLabel>
       </div>
-
       <div className="my-5">
         <FloatLabel>
           <InputText
@@ -38,21 +116,27 @@ export const RegisterPage = () => {
             name="password_confirmation"
             className="w-full"
             type="password"
+            value={password_confirmation}
+            onChange={onChange}
           />
           <label htmlFor="password_confirmation">Repetir Contraseña</label>
+          {errors.password_confirmation && (
+            <p className="text-red-500">
+              {errors.password_confirmation[0]}
+            </p>
+          )}
         </FloatLabel>
       </div>
-
       <div className="my-5 ">
         <button
           type="submit"
-          className="bg-blue-600 text-white rounded-lg px-3 p-2 "
+          className="bg-blue-600 text-white rounded-lg px-3 p-2 w-full"
         >
           Registrarse
         </button>
       </div>
       <div className="my-5">
-        <Link to={"/auth/login"} className="text-blue-600 text-sm">
+        <Link to="/auth/login" className="text-blue-600 text-sm">
           ¿Ya tienes una cuenta? Inicia Sesión
         </Link>
       </div>
