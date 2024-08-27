a2439a54c6ff9328e6a121e42e947a5d01d66faf
Leodomi <Leodomi15@gmail.com>
2024-08-27 19:45:55 -0400

    iniciando la funcion del login

--- src/views/Auth/LoginPage.jsx
+++ src/views/Auth/LoginPage.jsx
@@ -4,26 +4,37 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const LoginPage = () => {
-  const [password, setPassword] = useState();
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('')
+  const [errors, setErrors] = useState([])
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+  }
   return (
-    <form className="border border-gray-200 p-6 mx-auto md:w-2/3 lg:w-3/4 shadow-xl rounded-xl bg-white">
+    <form className="border border-gray-200 p-6 mx-auto md:w-2/3 lg:w-3/4 shadow-xl rounded-xl bg-white" onSubmit={onSubmit}>
 
       <h1 className="text-3xl font-bold text-center">Iniciar Sesión</h1>
       <div className="my-5">
         <FloatLabel>
-          <InputText id="email" name="email" className="w-full" type="email"/>
+          <InputText id="email" name="email" className="w-full" type="email" value={email} onChange={onChange}/>
           <label htmlFor="email">Email</label>
         </FloatLabel>
       </div>
       <div className="my-5">
         <FloatLabel className="w-full">
-          <InputText id="password" name="password" className="w-full" type="password"/>
+          <InputText id="password" name="password" className="w-full" type="password" value={password} onChange={onChange} />
           <label htmlFor="password">Contraseña</label>
         </FloatLabel>
       </div>
       
       <div className="my-5">
-      <button type="submit" className="bg-blue-600 text-white rounded-lg px-3 p-2 ">
+      <button type="submit" className="bg-blue-600 text-white rounded-lg px-3 p-2 w-full">
             Iniciar Sesión
       </button>
       </div>
